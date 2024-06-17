ebe400e11544344a2cbc18659a1b403e2de889d0
Aveek <aveek.d22@gmail.com>
2024-06-17 16:24:37 +0200

    Update agnoster theme

---
+++ scripts/oh-my-zsh/customize-agnoster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Find the default installation directory for oh-my-zsh
+Normally, it should be at ~/.oh-my-zsh
+Find the directory in which the themes are located.
+Remove the `agnoster.zsh-theme` and create a symlink to the one
+in this repository.
+
+Ask me to source my terminal again.
